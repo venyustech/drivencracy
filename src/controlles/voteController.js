070b8c3ad038fed7eac1188677b2999256a4dda0
Gardenia Georgia Barbosa De Siqueira <venyustech@gmail.com>
2022-02-17 22:52:56 -0300

    feat: postVoteById by /choice/:id/vote route

--- src/controlles/voteController.js
+++ src/controlles/voteController.js
@@ -4,4 +4,19 @@ import dayjs from "dayjs";
 
 export async function postVoteById(req, res) {
 
+    try {
+        let todayDate = dayjs().format('YYYY-MM-DD HH:mm');
+        const choice = await db.collection("choices").findOne({ id: parseInt(req.params.id) });
+
+        if (choice) {
+            await db.collection("vote").insertOne({
+                createdAt: todayDate,
+                choiceId: req.params.id,
+            });
+            res.status(201).send("OK");
+        } else res.status(404).send("escolha não existente");
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).send(error.message);
+    }
 }
